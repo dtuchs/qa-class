13dc49e1986caf0cef5314e02e58e72cb60026dc
dtuchs <d.tuchs@propellerads.net>
2020-02-04 17:51:51 +0300

    04.02

--- src/test/java/com/propellerads/test/SelenideSimpleTest.java
+++ src/test/java/com/propellerads/test/SelenideSimpleTest.java
@@ -1,37 +1,55 @@
 package com.propellerads.test;
 
+import com.codeborne.selenide.WebDriverRunner;
 import com.propellerads.page.YandexMainPage;
 import com.propellerads.util.Language;
 import com.propellerads.util.Period;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EnumSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
 class SelenideSimpleTest {
-    
 
-    static Stream<Arguments> demoTest() {
-        return Stream.of(
-                Arguments.of(Period.DAY, Language.EN),
-                Arguments.of(Period.DAY, Language.RU),
-                Arguments.of(Period.WEEKS, Language.EN),
-                Arguments.of(Period.MONTH, Language.RU)
-        );
+    private YandexMainPage yandexMainPage;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Запустили два теста!");
     }
 
-    @EnumSource(value = Period.class, names = {"DAY"})
-    @ParameterizedTest
-    void demoTest(Period period) {
-        open("http://ya.ru", YandexMainPage.class)
-                .getSearchComponent()
+    @BeforeEach
+    void setup() {
+        yandexMainPage = open("http://ya.ru/", YandexMainPage.class);
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        WebDriverRunner.closeWebDriver();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Закончили два теста!");
+    }
+
+    @Test
+    void firstTest() {
+        yandexMainPage.getSearchComponent()
+                .search("searchQuery")
+                .checkResultsCount(10);
+    }
+
+    @Test
+    void secondTest() {
+        yandexMainPage.getSearchComponent()
                 .search("searchQuery")
                 .checkResultsCount(10)
                 .openAdvancedSettings()
-                .choosePeriod(period)
+                .choosePeriod(Period.WEEKS)
                 .chooseLanguage(Language.EN)
                 .submitAdvancedSettings()
                 .checkResultsCount(10);
